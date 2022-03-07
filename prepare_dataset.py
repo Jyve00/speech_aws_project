cf831d6dab2e2fe8c9c399b860f22b2534160dae
Stephen William <swilliam.productions@gmail.com>
2022-03-07 16:58:34 -0500

    class

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -11,4 +11,27 @@ def prepare_dataset(dataset_path, json_path, n_mfcc=13, hop_length=512, n_fft=20
     # data dictionary 
     data = {
         "mappings": [],
+        "labels": [], 
+        "MFCCs": [],
+        "files": []
     }
+
+    # loop through all the sub-dirs
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+
+        # need to ensure that we're not at root level 
+        if dirpath is not dataset_path:
+            # update mappings 
+            category = dirpath.split("/")[-1] # dataset/down -> [dataset, down]
+            data['mappings'].append(category)
+
+            # loop through all the filenames and extract MFCCs
+            for f in filenames: 
+
+                # get file path 
+
+                # load audio file 
+
+                # 
+
+
