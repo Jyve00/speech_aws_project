a1374b2f110d4897f04260af6bf347083130ee8e
Stephen William <swilliam.productions@gmail.com>
2022-03-09 09:44:47 -0500

    error

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -4,9 +4,18 @@ import os
 
 DATASET_PATH = "data"
 JSON_PATH = "data.json"
-SAMPLES_TO_CONSIDER = 8000
+SAMPLES_TO_CONSIDER = 16000
 
 def prepare_dataset(dataset_path, json_path, n_mfcc=13, hop_length=512, n_fft=2048):
+    """Extract MFCCs from dataset and saves them into a json file. 
+
+    Args:
+        param dataset_path (str): Path to Dataset
+        json_path (_type_): _description_
+        n_mfcc (int, optional): _description_. Defaults to 13.
+        hop_length (int, optional): _description_. Defaults to 512.
+        n_fft (int, optional): _description_. Defaults to 2048.
+    """
 
     # data dictionary 
     data = {
@@ -15,6 +24,7 @@ def prepare_dataset(dataset_path, json_path, n_mfcc=13, hop_length=512, n_fft=20
         "MFCCs": [],
         "files": []
     }
+
 
     # loop through all the sub-dirs
     for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
@@ -53,5 +63,7 @@ def prepare_dataset(dataset_path, json_path, n_mfcc=13, hop_length=512, n_fft=20
     with open(json_path, "w") as fp: 
         json.dump(data, fp, indent=4)
 
-        
+if __name__ == "__main__":
+    prepare_dataset(DATASET_PATH, JSON_PATH)
+
 
